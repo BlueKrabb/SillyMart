c99778005e4f1c550db36d9ec74e2b052089261f
Blue Krabb <shambale.abasi8@gmail.com>
2026-06-24 21:37:49 +0300

    added player aniamtion

--- Assets/Scripts/player_movemnt.gd
+++
@@ -1,9 +0,0 @@
-extends CharacterBody2D
-
-var speed = 50  # speed in pixels/sec
-
-func _physics_process(delta):
-	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	velocity = direction * speed
-
-	move_and_slide()
